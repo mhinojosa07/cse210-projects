41c55896922cd1def5634199471f6b2e21c228ab
mhinojosa07 <nick_h86@hotmail.com>
2025-06-16 19:22:42 -0600

    Finishd assignment

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create shape instances
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Circle circle = new Circle("Green", 3);
+
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        // Display color and area for each shape
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():0.00}");
+            Console.WriteLine();
+        }
     }
 }
